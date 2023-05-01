54c57bc2d14f8d29edff01d5c3f50fa67b5bf9b2
oleksii-zviezdin <oleksii.zviezdin@gmail.com>
2023-05-01 21:25:04 +0200

    add options checked "title" props
    add operato if to checked value of props of "title"

--- src/components/Statistics/Statistic.jsx
+++ src/components/Statistics/Statistic.jsx
@@ -2,11 +2,17 @@ import { StatisticsBoard } from "./StatisticBoard/StatisticsBoard"
 import data from "../../data.json"
 import css from './Statistics.module.css'
 
-export const Statistics = ({title}) => {
-    return (
-        <section className={css.statistics}>
-            <h2 className={css.title}>{title}</h2>
-            <StatisticsBoard statisticData={data} />
-        </section>
-    )
+export const Statistics = ({ title }) => {
+    if (title === null || title === "" || title === undefined || !title) {
+        return  <section className={css.statistics}>
+                    <StatisticsBoard statisticData={data} />
+                </section>
+    } else {
+        return (
+            <section className={css.statistics}>
+                <h2 className={css.title}>{title}</h2>
+                <StatisticsBoard statisticData={data} />
+            </section>
+        )
+    }
 }
